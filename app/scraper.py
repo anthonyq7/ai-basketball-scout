a01b718b9b8fb9cb8fe6237448620498f896e9f9
anthonyq7 <a.j.qin@wustl.edu>
2025-08-25 18:32:56 -0500

    changed imports again

--- app/scraper.py
+++ app/scraper.py
@@ -3,7 +3,7 @@ import pandas as pd
 import httpx
 from io import StringIO  
 from bs4 import BeautifulSoup, Comment
-from app.models import Player
+from models import Player
 from nba_api.stats.static import players as nba_players
 from fastapi import HTTPException
 from typing import Optional
